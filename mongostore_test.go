ec0d0f2d24d9327515be09e69d0dced4ff12fde2
Safia Abdalla <safia@safia.rocks>
2015-10-13 21:01:35 -0500

    Added tests for NewMongoStore function

--- mongostore_test.go
+++ mongostore_test.go
@@ -3,16 +3,16 @@ package registry
 import "testing"
 
 func TestNewMongoStore(t *testing.T) {
-    var MONGODB_URL string = "http://localhost:27017"
+    var MONGODB_URL string = "127.0.0.1"
     var MONGODB_DB string = "binder_registery_tests"
     var MONGODB_COL string = "templates"
-    connection, error := NewMongoStore(MONGODB_URL, MONGODB_DB, MONGODB_COL)
-    
-    if (error != nil) {
-        t.Error("NewMongoStore raised error:", error);
+    store := NewMongoStore(MONGODB_URL, MONGODB_DB, MONGODB_COL)
+
+    if (store.err != nil) {
+        t.Error("NewMongoStore raised error:", store.err);
     }
 
-    if (connection.FullName != (MONGODB_DB + "." + MONGODB_COL)) {
-       t.Error("Connected to wrong database:", connection.FullName) 
+    if (store.connection.FullName != (MONGODB_DB + "." + MONGODB_COL)) {
+       t.Error("Connected to wrong database:", store.connection.FullName)
     }
 }
