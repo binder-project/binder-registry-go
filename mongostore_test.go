d53080ac1091f518feb8a123f8d5073d6c9f7040
Safia Abdalla <safia@safia.rocks>
2015-10-13 22:16:11 -0500

    Added tests for RegisterTemplate on Mongo

--- mongostore_test.go
+++ mongostore_test.go
@@ -16,3 +16,28 @@ func TestNewMongoStore(t *testing.T) {
        t.Error("Connected to wrong database:", store.connection.FullName)
     }
 }
+
+func TestMongoRegisterTemplate(t *testing.T) {
+    var MONGODB_URL string = "127.0.0.1"
+    var MONGODB_DB string = "binder_registery_tests"
+    var MONGODB_COL string = "templates"
+    store := NewMongoStore(MONGODB_URL, MONGODB_DB, MONGODB_COL)
+
+   tmpl := Template{
+        Name: "MongoTest",
+        ImageName: "jupyter/mongo_test",
+    }
+
+    registered, err := store.RegisterTemplate(tmpl)
+
+    if (err != nil) {
+        t.Error("Error when registering template: ", err)
+    }
+
+    if (registered == Template{}) {
+        t.Error("Template was not registered properly")
+    }
+
+    equals(t, registered.Name, tmpl.Name)
+    equals(t, registered.ImageName, tmpl.ImageName)
+}
